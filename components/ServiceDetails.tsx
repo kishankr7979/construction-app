db4b1bf3c0729005206e70e0420e7cb5c3393f98
Tarun <taruuunmishra300@gmail.com>
2022-06-11 15:10:09 +0530

    added new screen

--- components/ServiceDetails.tsx
+++ components/ServiceDetails.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import {ScrollView, View, Text} from 'react-native';
+import {ScrollView,StyleSheet, View, Text,Image, ImageBackground} from 'react-native';
+import RupeeIcon from 'react-native-vector-icons/FontAwesome';
+import { color } from 'react-native-elements/dist/helpers';
 interface WebviewProps {
     route: any;
     navigation: any,
@@ -8,10 +10,73 @@ const ServiceDetails = ({route, navigation}: WebviewProps) => {
     const {packageDetails} = route.params;
     console.log(packageDetails);
     return (
-        <ScrollView>
-            
-        </ScrollView>
+        <View style={styles.conatiner}>
+            <View  style={styles.subContainer}>
+                <View style={styles.descContainer}>
+                <ImageBackground source={require('../assets/paint.jpeg')} resizeMode='stretch' style={{width: '100%', height: '100%', borderRadius: 15, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <Text style={styles.text}>{packageDetails.flatType}BHK</Text>
+                    <View style={styles.iconImg}>
+                        <RupeeIcon name='rupee' size={25} color="red"/>
+                        <Text style={styles.subText}>:{packageDetails.price}</Text>
+                        </View>
+                    <Text style={styles.subText}>TimeSpan:{packageDetails.timeline}</Text>
+                    </ImageBackground> 
+                </View>              
+            </View>
+            <ScrollView>
+                <Text style={{fontSize: 40}}>hi</Text>
+                <Text style={{fontSize: 40}}>hi</Text>
+                <Text style={{fontSize: 40}}>hi</Text>
+                <Text style={{fontSize: 40}}>hi</Text>
+            </ScrollView>
+        </View>
+        
     )
 }
+
+const styles = StyleSheet.create({
+    conatiner:{
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+    },
+    subContainer: {
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        width: '100%',
+        backgroundColor: 'grey',
+        height:'69%',
+        borderBottomRightRadius:25,
+        borderBottomLeftRadius:25,
+        borderColor: '#2196F3',
+        borderWidth: 2,
+    },
+    descContainer:{
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:'#FFFFFF',
+        width: '90%',
+        height:'80%',
+        borderRadius:20,
+    },
+    text:{
+        fontSize:30,
+        fontWeight:'bold',
+        color:'black'
+    },
+    subText:{
+        fontSize:20,
+        color:'black',
+        fontWeight:'bold'
+        },
+    iconImg:{
+        display:'flex',
+        flexDirection:'row'
+    }
+})
 
 export default ServiceDetails
